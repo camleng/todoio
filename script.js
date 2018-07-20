cfe66098103bfe0a25ec919e082a62705336e33c
Cameron Lengerich <cam.lengerich@monroevalley.com>
2018-07-20 07:59:04 -0400

    feat: Navbar with a initial layout

---
+++ script.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        todos: [{
+                title: 'Laundry',
+                complete: false
+            },
+            {
+                title: 'Sleep',
+                complete: false
+            }
+        ]
+    }
+});
